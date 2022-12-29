c3ccb74aea56bf92e58dd344fe26e61526e6d37f
James Conway <71008025+ieUlysses@users.noreply.github.com>
2022-12-29 18:36:40 +0000

    pausing progress. found bug

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className=" p-5 bg-black">
+            <p className="text-white">Header</p>
+        </header>
+    )
+}
+
+export default Header
