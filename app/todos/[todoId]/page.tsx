35241b87ca9cf328fc188bf41fafc3ecaba53632
James Conway <71008025+ieUlysses@users.noreply.github.com>
2023-01-26 17:59:26 +0000

    Created dynamic rooting, finishing up for today

---
+++ app/todos/[todoId]/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Todo } from '../../../typings';
+
+type PageProps = {
+    params: {
+        todoId: string;
+
+    }
+}
+
+const fetchTodo = async (todoId: string) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+
+    const todo: Todo = await res.json()
+    return todo
+
+}
+
+
+async function TodoPage({ params: { todoId } }: PageProps) {
+
+    const todo = await fetchTodo(todoId)
+
+    return (
+        <div className='p-10 bg-black text-white border-3 m-2 shadow-md rounded-lg'>
+            <h1>
+                {todo.title}
+            </h1>
+            <p>
+                # {todo.id}
+            </p>
+
+            <p>
+                Status: {todo.completed ? "Outstanding" : "Done"}
+            </p>
+
+            <p className='border-t border-white mt-5 text-right border-dashed'>
+                User : {todo.userId}
+            </p>
+        </div>
+    )
+}
+
+export default TodoPage
